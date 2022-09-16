eb629681cc335d0c4d672a6d37adbc57d0bd0c27
kate <k-b-v@yandex.ru>
2022-09-16 19:32:58 +0300

    Three biger number

---
+++ Three number/Program.cs
@@ -0,0 +1,40 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if(a > max ) max = a;
+if(b > max ) max = b;
+if(c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int d = 44;
+int e = 5;
+int f = 78;
+
+int max2 = d;
+
+if(d > max2 ) max2 = d;
+if(e > max2 ) max2 = e;
+if(f > max2 ) max2 = f;
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+int g = 22;
+int j = 3;
+int k = 9;
+
+int max3 = g;
+
+if(g > max3 ) max3 = g;
+if(j > max3 ) max3 = j;
+if(k > max3 ) max3 = k;
+
+
+Console.Write("max3 = ");
+Console.WriteLine(max3);
+
