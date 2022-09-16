68cc9aa5708109616a6f80cc4a37794e16f7dc6a
kate <k-b-v@yandex.ru>
2022-09-16 19:21:49 +0300

    Program 1

---
+++ Bigger/Program.cs
@@ -0,0 +1,32 @@
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+
+if(a > max ) max = a;
+if(b > max ) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+int c = 2;
+int d = 10;
+
+int max2 = c;
+
+if(c > max2 ) max2 = c;
+if(d > max2 ) max2 = d;
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+int e = -9;
+int f = -3;
+
+int max3 = e;
+
+if(e > max3 ) max3 = e;
+if(f > max3 ) max3 = f;
+
+Console.Write("max3 = ");
+Console.WriteLine(max3);
