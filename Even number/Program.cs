305564f61d08d171f5f3139e7588cd7a3dd46b25
kate <k-b-v@yandex.ru>
2022-09-18 17:57:09 +0300

    even number

---
+++ Even number/Program.cs
@@ -0,0 +1,40 @@
+﻿int a = 4;
+
+if (a % 2 == 0)
+
+{
+
+    System.Console.WriteLine("число четное");
+
+}
+else
+{
+    System.Console.WriteLine("число нечетное");
+
+}
+
+int b = -3;
+
+if (b % 2 == 0){
+
+    System.Console.WriteLine("число четное");
+
+}
+else
+{
+    System.Console.WriteLine("число нечетное");
+
+}
+int c = 7;
+if (c % 2 == 0)
+{
+
+    System.Console.WriteLine("число четное");
+
+}
+else
+{
+    System.Console.WriteLine("число нечетное");
+
+}
+
