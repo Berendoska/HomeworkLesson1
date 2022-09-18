c43373556a293731ee37ee769cd2419f72548c4b
kate <k-b-v@yandex.ru>
2022-09-18 19:03:18 +0300

    all even for 5 and 8

---
+++ Number/Program.cs
@@ -0,0 +1,38 @@
+﻿int N = 5;
+int a = 1;
+
+while (a <= N) 
+{
+    if (a % 2 == 0)
+    {
+         System.Console.WriteLine(a);
+      
+    
+    }
+a++;
+
+
+    
+}
+
+Console.WriteLine("Следующая задача");
+int C = 8;
+int b = 1;
+
+while (b <= C) 
+{
+    if (b % 2 == 0)
+    {
+         System.Console.WriteLine(b);
+      
+    
+    }
+b++;
+    
+}
+
+
+
+
+
+
